5a992b72d910816fb7cb00790d0264f34ea11067
alexandrebbch <alexandre.bbch@eduge.ch>
2024-01-30 15:46:52 +0100

    Save

---
+++ Assets/Scripts/NodeGraph/Editor/RoomNodeGraphEditor.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+public class RoomNodeGraphEditor : EditorWindow
+{
+ [MenuItem ("Room Node Graph Editor", menuItem = "Window/Dungeon Editor/Room Node Graph Editor")]
+
+    private static void OpenWindow()
+    {
+
+    }
+
+
+
+}
